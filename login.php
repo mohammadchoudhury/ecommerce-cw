e6a52656a88e7dd84e6be3105e023fc39766d10d
Mohammad Choudhury <mohammadchoudhury@users.noreply.github.com>
2017-02-21 15:28:57 +0000

    Created login file

---
+++ login.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link type="text/css" rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<?php include 'header.php'; ?>
+	<div id="banner">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-7">
+					<h1>Login</h1>
+				</div>
+				<div class="col-md-5">
+					<ul class="breadcrumb">
+						<li><a href="index.php">Home</a>
+						</li>
+						<li>Login</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<section class="gray-bg text-center">
+		<div class="container">
+			<div class="row">
+				<h1>Login</h1>
+				<p>Login in to your account</p>
+			</div>
+		</div>
+	</section>
+
+	<?php include 'footer.php'; ?>
+
+	<script src="js//jquery.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
