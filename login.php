69c2dff38bc09fcec4ab65bd5c5c17905d8ceb70
Mohammad Choudhury <mohammadchoudhury@users.noreply.github.com>
2017-02-23 17:37:22 +0000

    Improved validate login js

--- login.php
+++ login.php
@@ -32,7 +32,7 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-md-4 col-md-push-4">
-					<form id="login_form" method="post" action="login.php" onsubmit="return validateLogin(this.value);">
+					<form id="login_form" method="post" action="login.php" onsubmit="return validateLogin();">
 						<div class="panel panel-default text-center">
 							<div class="panel-heading">
 								<h2 class="text-uppercase">Login</h2>
@@ -85,7 +85,7 @@
 			}
 			console.log(errors == '');
 			document.getElementById("error").innerHTML = errors;
-			return errors == '';
+			return !errors;
 		}
 	</script>
 
