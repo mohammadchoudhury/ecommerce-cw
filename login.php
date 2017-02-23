fd153a49fa766e3d503e2e5cc7199dbdebcf3e82
Mohammad Choudhury <mohammadchoudhury@users.noreply.github.com>
2017-02-23 09:43:21 +0000

    Added basic js validation for login

--- login.php
+++ login.php
@@ -23,13 +23,12 @@
 						</li>
 						<li>Login</li>
 					</ul>
-
 				</div>
 			</div>
 		</div>
 	</div>
 
-	<section class="white-bg">
+<!-- 	<section class="white-bg">
 		<div class="container">
 			<div class="row">
 				<div class="col-md-4">
@@ -113,24 +112,24 @@
 				</div>
 			</div>
 		</div>
-	</section>
-
+	</section> -->
 
 	<section class="gray-bg">
 		<div class="container">
 			<div class="row">
 				<div class="col-md-4">
-					<div class="panel panel-default text-center">
-						<div class="panel-heading">
-							<h2 class="text-uppercase">Login</h2>
-							<p class="lead text-center">Already our customer?</p>
-						</div>
-						<div class="panel-body">
-							<form id="login_form">
+					<form id="login_form" method="post" action="login.php" onsubmit="return validateLogin(this.value);">
+						<div class="panel panel-default text-center">
+							<div class="panel-heading">
+								<h2 class="text-uppercase">Login</h2>
+								<p class="lead text-center">Already our customer?</p>
+							</div>
+							<div class="panel-body">
+								<div id="error"></div>
 								<div class="form-group">
 									<div class="input-group">
-									  <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-									  <input id="email" type="text" class="form-control" name="email" placeholder="Email">
+										<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+										<input id="email" type="text" class="form-control" name="email" placeholder="Email">
 									</div>
 								</div>
 								<div class="form-group">
@@ -139,16 +138,16 @@
 										<input id="password" type="password" class="form-control" name="password" placeholder="Password">
 									</div>
 								</div>
-							</form>
+							</div>
+							<div class="panel-footer">
+								<button type="submit" class="btn btn-primary" form="login_form">Log in <i class="glyphicon glyphicon-log-in"></i></button>
+								<button type="reset" class="btn btn-danger" form="login_form">Reset <i class="glyphicon glyphicon-erase"></i></button>
+							</div>
 						</div>
-						<div class="panel-footer">
-							<button type="submit" class="btn btn-primary" form="login_form">Log in <i class="glyphicon glyphicon-log-in"></i></button>
-							<button type="reset" class="btn btn-danger" form="login_form">Reset <i class="glyphicon glyphicon-erase"></i></button>
-						</div>
-					</div>
+					</form>
 				</div>
 
-				<div class="col-md-4">
+				<!-- <div class="col-md-4">
 					<div class="text-center">
 						<h2 class="text-uppercase">Login</h2>
 						<p class="lead">Already our customer?</p>
@@ -197,13 +196,13 @@
 							<button type="reset" class="btn btn-danger">Reset <i class="glyphicon glyphicon-erase"></i></button>
 						</form>
 					</div>
-				</div>
+				</div> -->
 			</div>
 		</div>
 	</section>
 
 
-	<style type="text/css">
+<!-- 	<style type="text/css">
 		.white-bg {
 			background-color: white;
 		}
@@ -293,12 +292,31 @@
 				</div>
 			</div>
 		</div>
-	</section>
+	</section> -->
 
 	<?php include 'footer.php'; ?>
 
 	<script src="js//jquery.js"></script>
 	<script src="js/bootstrap.min.js"></script>
+
+	<script type="text/javascript">
+		function validateLogin() {
+			var form = document.getElementById("login_form");
+			var email = form['email'].value;
+			var password = form['password'].value;
+			var errors = "";
+			if (!email || !password) {
+				errors += "<div class='alert alert-danger alert-dismissible'><button type='button' class='close' data-dismiss='alert'><span>&times;</span></button>Must enter username and password</div>";
+			}
+			var email_regex = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+			if (!email_regex.test(email)) {
+				errors += "<div class='alert alert-danger alert-dismissible'><button type='button' class='close' data-dismiss='alert'><span>&times;</span></button>Email address is invalid</div>";
+			}
+			console.log(errors == '');
+			document.getElementById("error").innerHTML = errors;
+			return errors == '';
+		}
+	</script>
 
 </body>
 </html>
