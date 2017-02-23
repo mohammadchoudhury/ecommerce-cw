7434eac3fe9c02f04ec37cc362551ba74509ee08
Mohammad Choudhury <mohammadchoudhury@users.noreply.github.com>
2017-02-23 17:22:34 +0000

    Login design chosen

--- login.php
+++ login.php
@@ -28,101 +28,15 @@
 		</div>
 	</div>
 
-<!-- 	<section class="white-bg">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-4">
-					<div class="panel panel-default text-center">
-						<div class="panel-heading">
-							<h2 class="text-uppercase">Login</h2>
-							<p class="lead text-center">Already our customer?</p>
-						</div>
-						<div class="panel-body">
-							<form id="login_form">
-								<div class="form-group">
-									<div class="input-group">
-									  <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-									  <input id="email" type="text" class="form-control" name="email" placeholder="Email">
-									</div>
-								</div>
-								<div class="form-group">
-									<div class="input-group">
-										<span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-										<input id="password" type="password" class="form-control" name="password" placeholder="Password">
-									</div>
-								</div>
-							</form>
-						</div>
-						<div class="panel-footer">
-							<button type="submit" class="btn btn-primary" form="login_form">Log in <i class="glyphicon glyphicon-log-in"></i></button>
-							<button type="reset" class="btn btn-danger" form="login_form">Reset <i class="glyphicon glyphicon-erase"></i></button>
-						</div>
-					</div>
-				</div>
-
-				<div class="col-md-4">
-					<div class="text-center">
-						<h2 class="text-uppercase">Login</h2>
-						<p class="lead">Already our customer?</p>
-						<hr style="border-color: #2c3e50;">
-						<div class="well">
-							<form action="login.php" method="post">
-								<div class="form-group">
-									<div class="input-group">
-									  <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-									  <input id="email" type="text" class="form-control" name="email" placeholder="Email">
-									</div>
-								</div>
-								<div class="form-group">
-									<div class="input-group">
-										<span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-										<input id="password" type="password" class="form-control" name="password" placeholder="Password">
-									</div>
-								</div>
-								<button type="submit" class="btn btn-primary">Log in <i class="glyphicon glyphicon-log-in"></i></button>
-								<button type="reset" class="btn btn-danger">Reset <i class="glyphicon glyphicon-erase"></i></button>
-							</form>
-						</div>
-					</div>
-				</div>
-
-				<div class="col-md-4">
-					<div class="well text-center">
-						<h2 class="text-uppercase">Login</h2>
-						<p class="lead">Already our customer?</p>
-						<hr style="border-color: #2c3e50;">
-						<form action="login.php" method="post">
-							<div class="form-group">
-								<div class="input-group">
-									<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-									<input id="email" type="text" class="form-control" name="email" placeholder="Email">
-								</div>
-							</div>
-							<div class="form-group">
-								<div class="input-group">
-									<span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-									<input id="password" type="password" class="form-control" name="password" placeholder="Password">
-								</div>
-							</div>
-							<hr style="border-color: #2c3e50;">
-							<button type="submit" class="btn btn-primary">Log in <i class="glyphicon glyphicon-log-in"></i></button>
-							<button type="reset" class="btn btn-danger">Reset <i class="glyphicon glyphicon-erase"></i></button>
-						</form>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section> -->
-
 	<section class="gray-bg">
 		<div class="container">
 			<div class="row">
-				<div class="col-md-4">
+				<div class="col-md-4 col-md-push-4">
 					<form id="login_form" method="post" action="login.php" onsubmit="return validateLogin(this.value);">
 						<div class="panel panel-default text-center">
 							<div class="panel-heading">
 								<h2 class="text-uppercase">Login</h2>
-								<p class="lead text-center">Already our customer?</p>
+								<p class="lead text-center">Already a customer?</p>
 							</div>
 							<div class="panel-body">
 								<div id="error"></div>
@@ -146,153 +60,10 @@
 						</div>
 					</form>
 				</div>
-
-				<!-- <div class="col-md-4">
-					<div class="text-center">
-						<h2 class="text-uppercase">Login</h2>
-						<p class="lead">Already our customer?</p>
-						<hr style="border-color: #2c3e50;">
-						<div class="well">
-							<form action="login.php" method="post">
-								<div class="form-group">
-									<div class="input-group">
-									  <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-									  <input id="email" type="text" class="form-control" name="email" placeholder="Email">
-									</div>
-								</div>
-								<div class="form-group">
-									<div class="input-group">
-										<span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-										<input id="password" type="password" class="form-control" name="password" placeholder="Password">
-									</div>
-								</div>
-								<button type="submit" class="btn btn-primary">Log in <i class="glyphicon glyphicon-log-in"></i></button>
-								<button type="reset" class="btn btn-danger">Reset <i class="glyphicon glyphicon-erase"></i></button>
-							</form>
-						</div>
-					</div>
-				</div>
-
-				<div class="col-md-4">
-					<div class="well text-center">
-						<h2 class="text-uppercase">Login</h2>
-						<p class="lead">Already our customer?</p>
-						<hr style="border-color: #2c3e50;">
-						<form action="login.php" method="post">
-							<div class="form-group">
-								<div class="input-group">
-									<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-									<input id="email" type="text" class="form-control" name="email" placeholder="Email">
-								</div>
-							</div>
-							<div class="form-group">
-								<div class="input-group">
-									<span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-									<input id="password" type="password" class="form-control" name="password" placeholder="Password">
-								</div>
-							</div>
-							<hr style="border-color: #2c3e50;">
-							<button type="submit" class="btn btn-primary">Log in <i class="glyphicon glyphicon-log-in"></i></button>
-							<button type="reset" class="btn btn-danger">Reset <i class="glyphicon glyphicon-erase"></i></button>
-						</form>
-					</div>
-				</div> -->
 			</div>
 		</div>
 	</section>
 
-
-<!-- 	<style type="text/css">
-		.white-bg {
-			background-color: white;
-		}
-	</style>
-
-	<section class="gray-bg">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-4">
-					<div class="panel panel-default text-center">
-						<div class="panel-heading">
-							<h2 class="text-uppercase">Login</h2>
-							<p class="lead text-center">Already our customer?</p>
-						</div>
-						<div class="panel-body">
-							<form id="login_form">
-								<div class="form-group">
-									<div class="input-group">
-									  <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-									  <input id="email" type="text" class="form-control" name="email" placeholder="Email">
-									</div>
-								</div>
-								<div class="form-group">
-									<div class="input-group">
-										<span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-										<input id="password" type="password" class="form-control" name="password" placeholder="Password">
-									</div>
-								</div>
-							</form>
-						</div>
-						<div class="panel-footer">
-							<button type="submit" class="btn btn-primary" form="login_form">Log in <i class="glyphicon glyphicon-log-in"></i></button>
-							<button type="reset" class="btn btn-danger" form="login_form">Reset <i class="glyphicon glyphicon-erase"></i></button>
-						</div>
-					</div>
-				</div>
-
-				<div class="col-md-4">
-					<div class="text-center">
-						<h2 class="text-uppercase">Login</h2>
-						<p class="lead">Already our customer?</p>
-						<hr style="border-color: #2c3e50;">
-						<div class="well white-bg">
-							<form action="login.php" method="post">
-								<div class="form-group">
-									<div class="input-group">
-									  <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-									  <input id="email" type="text" class="form-control" name="email" placeholder="Email">
-									</div>
-								</div>
-								<div class="form-group">
-									<div class="input-group">
-										<span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-										<input id="password" type="password" class="form-control" name="password" placeholder="Password">
-									</div>
-								</div>
-								<button type="submit" class="btn btn-primary">Log in <i class="glyphicon glyphicon-log-in"></i></button>
-								<button type="reset" class="btn btn-danger">Reset <i class="glyphicon glyphicon-erase"></i></button>
-							</form>
-						</div>
-					</div>
-				</div>
-
-				<div class="col-md-4">
-					<div class="well text-center white-bg">
-						<h2 class="text-uppercase">Login</h2>
-						<p class="lead">Already our customer?</p>
-						<hr style="border-color: #2c3e50;">
-						<form action="login.php" method="post">
-							<div class="form-group">
-								<div class="input-group">
-									<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-									<input id="email" type="text" class="form-control" name="email" placeholder="Email">
-								</div>
-							</div>
-							<div class="form-group">
-								<div class="input-group">
-									<span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-									<input id="password" type="password" class="form-control" name="password" placeholder="Password">
-								</div>
-							</div>
-							<hr style="border-color: #2c3e50;">
-							<button type="submit" class="btn btn-primary">Log in <i class="glyphicon glyphicon-log-in"></i></button>
-							<button type="reset" class="btn btn-danger">Reset <i class="glyphicon glyphicon-erase"></i></button>
-						</form>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section> -->
 
 	<?php include 'footer.php'; ?>
 
