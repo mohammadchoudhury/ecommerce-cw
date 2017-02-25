0738bb21f406c649177f48a835df5b73f2cfefa5
Mohammad Choudhury <mohammadchoudhury@users.noreply.github.com>
2017-02-25 18:14:18 +0000

    Updated basket style

--- basket.php
+++ basket.php
@@ -34,50 +34,49 @@
 				<div class="col-md-9" id="basket">
 					<form method="post" action="">
 						<div class="panel panel-default">
-							<div class="panel-heading"><h4 class="text-uppercase">You currently have 3 item(s) in your basket.</h4></div>
-							<!-- <div class="panel-body"> -->
-								<div class="table-responsive">
-									<table class="table table-bordered table-striped">
-										<thead>
-											<tr>
-												<th colspan="2">Product</th>
-												<th>Quantity</th>
-												<th>Unit price</th>
-												<th>Discount</th>
-												<th colspan="4">Total</th>
-											</tr>
-										</thead>
-										<tbody>
-											<tr>
-												<td class="text-center"><a href="#"><img class="img-thumbnail" src="http://www.freeiconspng.com/uploads/car-icon-27.png"></a></td>
-												<td><a href="#">BMW 3 Series M Sport</a></td>
-												<td><input class="form-control" type="number" min="1" max="9" maxlength="1" value="2"></td>
-												<td>&pound;10,000.00</td>
-												<td>&pound;0.00</td>
-												<td>&pound;20,000.00</td>
-												<td class="text-center"><a href="#"><i class="glyphicon glyphicon-trash"></i></a>
-												</td>
-											</tr>
-											<tr>
-												<td class="text-center"><a href="#"><img class="img-thumbnail" src="http://www.freeiconspng.com/uploads/car-icon-27.png"></a></td>
-												<td><a href="#">BMW 3 Series M Sport</a></td>
-												<td><input class="form-control" type="number" min="1" max="9" maxlength="1" value="1"></td>
-												<td>&pound;15,000.00</td>
-												<td>&pound;0.00</td>
-												<td>&pound;15,000.00</td>
-												<td class="text-center"><a href="#"><i class="glyphicon glyphicon-trash"></i></a>
-												</td>
-											</tr>
-										</tbody>
-										<tfoot>
-											<tr>
-												<th class="text-right" colspan="5">Subtotal</th>
-												<th colspan="2">&pound;35,000.00</th>
-											</tr>
-										</tfoot>
-									</table>
-								</div>
-							<!-- </div> -->
+							<div class="panel-heading"><h4>You currently have 3 item(s) in your basket</h4></div>
+							
+							<div class="table-responsive">
+								<table class="table table-bordered">
+									<thead>
+										<tr>
+											<th colspan="2">Product</th>
+											<th>Quantity</th>
+											<th>Unit price</th>
+											<th>Discount</th>
+											<th colspan="4">Total</th>
+										</tr>
+									</thead>
+									<tbody>
+										<tr>
+											<td class="text-center"><a href="#"><img class="img-thumbnail" src="http://www.freeiconspng.com/uploads/car-icon-27.png"></a></td>
+											<td><a href="#">BMW 3 Series M Sport</a></td>
+											<td><input class="form-control" type="number" min="1" max="9" maxlength="1" value="2"></td>
+											<td>&pound;10,000.00</td>
+											<td>&pound;0.00</td>
+											<td>&pound;20,000.00</td>
+											<td class="text-center"><a href="#"><i class="glyphicon glyphicon-trash"></i></a>
+											</td>
+										</tr>
+										<tr>
+											<td class="text-center"><a href="#"><img class="img-thumbnail" src="http://www.freeiconspng.com/uploads/car-icon-27.png"></a></td>
+											<td><a href="#">BMW 3 Series M Sport</a></td>
+											<td><input class="form-control" type="number" min="1" max="9" maxlength="1" value="1"></td>
+											<td>&pound;15,000.00</td>
+											<td>&pound;0.00</td>
+											<td>&pound;15,000.00</td>
+											<td class="text-center"><a href="#"><i class="glyphicon glyphicon-trash"></i></a>
+											</td>
+										</tr>
+									</tbody>
+									<tfoot>
+										<tr>
+											<th class="text-right" colspan="5">Subtotal</th>
+											<th colspan="2">&pound;35,000.00</th>
+										</tr>
+									</tfoot>
+								</table>
+							</div>
 							<div class="panel-footer clearfix">
 								<div class="pull-left">
 									<a href="#" class="btn btn-danger"><i class="glyphicon glyphicon-arrow-left"></i> Continue shopping</a>
@@ -98,28 +97,28 @@
 								<h3 class="text-uppercase">Order summary</h3>
 							</div>
 							<!-- <div class="panel-body"> -->
-								<div class="table-responsive">
-									<table class="table table-bordered table-striped">
-										<tbody>
-											<tr>
-												<td>Subtotal</td>
-												<th>&pound;35,000.00</th>
-											</tr>
-											<tr>
-												<td>Shipping and handling</td>
-												<th>&pound;0.00</th>
-											</tr>
-											<tr>
-												<td>Tax</td>
-												<th>&pound;7,000.00</th>
-											</tr>
-											<tr class="total">
-												<td>Total</td>
-												<th>&pound;42,000.00</th>
-											</tr>
-										</tbody>
-									</table>
-								</div>
+							<div class="table-responsive">
+								<table class="table table-bordered table-striped">
+									<tbody>
+										<tr>
+											<td>Subtotal</td>
+											<th>&pound;35,000.00</th>
+										</tr>
+										<tr>
+											<td>Shipping and handling</td>
+											<th>&pound;0.00</th>
+										</tr>
+										<tr>
+											<td>Tax</td>
+											<th>&pound;7,000.00</th>
+										</tr>
+										<tr class="total">
+											<td>Total</td>
+											<th>&pound;42,000.00</th>
+										</tr>
+									</tbody>
+								</table>
+							</div>
 							<!-- </div> -->
 						</div>
 						<div class="panel panel-default">
