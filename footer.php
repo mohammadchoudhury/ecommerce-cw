dee2ae153b1e540e4bc1f4cc319bed830f4290aa
Mohammad Choudhury <mohammadchoudhury@users.noreply.github.com>
2017-04-22 20:23:57 +0100

    Updated the footer to include links and a mailing list form

--- footer.php
+++ footer.php
@@ -1,3 +1,59 @@
-<section class="dark-blue-bg text-center">
-	<h3>Copyright &copy; 2017 - Mohammad Choudhury</h3>
-</section>
+<style type="text/css">
+	footer {
+		padding-top: 20px;
+	}
+
+	footer ul {
+		list-style: none;
+		padding-left: 0;
+	}
+
+	footer a:hover {
+		text-decoration: none;
+		color: white;
+	}
+
+</style>
+<footer class="dark-blue-bg">
+	<div class="container">
+		<div class="col-xs-4 col-sm-3">
+			<h3>Quick Links</h3>
+			<ul>
+				<li><a href="#">List of Cars</a></li>
+				<li><a href="#">Contact Us</a></li>
+				<li><a href="#">About Us</a></li>
+				<li><a href="#">My Account</a></li>
+			</ul>
+		</div>
+		<div class="col-xs-4 col-sm-3">
+			<h3>Latest Cars</h3>
+			<ul>
+				<li><a href="car_details.php?id=30">BMW 7 Series</a></li>
+				<li><a href="car_details.php?id=27">Pagani Zonda F</a></li>
+				<li><a href="car_details.php?id=26">Mitsubishi Outlander</a></li>
+				<li><a href="car_details.php?id=25">Mercedes-Benz S-Class</a></li>
+			</ul>
+		</div>
+		<div class="col-xs-4 col-sm-3">
+			<h3>Popular Cars</h3>
+			<ul>
+				<li><a href="car_details.php?id=21">Lamborghini Gallardo</a></li>
+				<li><a href="car_details.php?id=18">Ferrari 488 Spider</a></li>
+				<li><a href="car_details.php?id=10">Bently Continental GT</a></li>
+				<li><a href="car_details.php?id=17">Chevrolet Camaro</a></li>
+			</ul>
+		</div>
+		<div class="col-sm-3">
+			<h3>Join the mailing list</h3>
+			<div class="row">
+				<form>
+					<input type="text" class="form-control" placeholder="Email ">
+					<button class="btn  btn-primary form-control" type="submit">Join mailing list </button>
+				</form>
+			</div>
+		</div>
+	</div>
+	<div class="text-center bg-primary" style="padding: 15px 0; margin-top: 30px;">
+		<h4>Copyright &copy; 2017 - Mohammad Choudhury</h4>
+	</div>
+</footer>
