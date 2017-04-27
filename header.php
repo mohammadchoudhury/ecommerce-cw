46d6dd2cdb704b720d35f04987eb7c780d53c0c0
Mohammad Choudhury <mohammadchoudhury@users.noreply.github.com>
2017-04-27 11:58:57 +0100

    Updated links in header

--- header.php
+++ header.php
@@ -47,8 +47,8 @@
 						?>
 					</ul>
 				</li>
-				<li><a href="#">Contact Us</a></li>
-				<li><a href="#">About Us</a></li>
+				<li><a href="contact.php">Contact Us</a></li>
+				<li><a href="about.php">About Us</a></li>
 			</ul>
 			<hr class="visible-xs">
 			<ul class="nav navbar-nav navbar-right">
